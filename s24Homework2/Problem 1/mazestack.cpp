b43821099a1a7668202e5ac991d81d5e121543f5
Cameron Maiden <cmaiden42@g.ucla.edu>
2024-04-29 19:58:07 -0700

    *Implemented the pathExists function using a stack

--- s24Homework2/Problem 1/mazestack.cpp
+++ s24Homework2/Problem 1/mazestack.cpp
@@ -1,5 +1,5 @@
 //
-//  main.cpp
+//  mazestack.cpp
 //  s24Homework2
 //
 //  Created by Cameron Maiden on 4/28/24.
@@ -12,6 +12,38 @@
 bool pathExists(char maze[][10], int sr, int sc, int er, int ec);
          // Return true if there is a path from (sr,sc) to (er,ec)
          // through the maze; return false otherwise
+
+///////////////////////////////////////////////////////////////////////////
+//  main function
+///////////////////////////////////////////////////////////////////////////
+
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int main()
+{
+    char maze[10][10] = {
+        // 0   1   2   3   4   5   6   7   8   9
+        { 'X','X','X','X','X','X','X','X','X','X' }, // 0
+        { 'X','.','.','.','X','.','.','X','.','X' }, // 1
+        { 'X','.','X','X','X','.','.','.','.','X' }, // 2
+        { 'X','.','X','.','X','X','X','X','.','X' }, // 3
+        { 'X','X','X','.','.','.','.','.','.','X' }, // 4
+        { 'X','.','.','.','X','.','X','X','.','X' }, // 5
+        { 'X','.','X','.','X','.','.','X','.','X' }, // 6
+        { 'X','.','X','X','X','X','.','X','.','X' }, // 7
+        { 'X','.','.','X','.','.','.','X','.','X' }, // 8
+        { 'X','X','X','X','X','X','X','X','X','X' }  // 9
+    };
+    
+    if (pathExists(maze, 8,1, 1,8))
+        cout << "Solvable!" << endl;
+    else
+        cout << "Out of luck!" << endl;
+    
+}
 
 ///////////////////////////////////////////////////////////////////////////
 //  Class implementations
@@ -32,62 +64,58 @@ private:
 //  Function implementations
 ///////////////////////////////////////////////////////////////////////////
 
-/// Push the starting coordinate (sr,sc) onto the coordinate stack and
-///     update maze[sr][sc] to indicate that the algorithm has encountered
-///     it (i.e., set maze[sr][sc] to have a value other than '.').
-/// While the stack is not empty,
-/// {   Pop the top coordinate off the stack. This gives you the current
-///         (r,c) location that your algorithm is exploring.
-///     If the current (r,c) coordinate is equal to the ending coordinate,
-///         then we've solved the maze so return true!
-///     Check each place you can move from the current cell as follows:
+bool pathExists(char maze[][10], int sr, int sc, int er, int ec)
+{
+    stack<Coord> coordStack;
+    coordStack.push(Coord(sr,sc));
+    maze[sr][sc] = '#';
+    
+    while(!coordStack.empty())
+    {
+        Coord curr = coordStack.top();
+        coordStack.pop();
+        
+        if (curr.r() == er && curr.c() == ec) // if the current coordinate is equal to the ending coordinate
+            return true;
+        
+// Check each place you can move from the current cell as follows:
 ///         If you can move EAST and haven't encountered that cell yet,
 ///             then push the coordinate (r,c+1) onto the stack and update
 ///             maze[r][c+1] to indicate the algorithm has encountered it.
+        if(maze[curr.r()][curr.c()+1] != 'X' && maze[curr.r()][curr.c()+1] != '#') // if EAST discoverable
+        {
+            coordStack.push(Coord(curr.r(), curr.c()+1));
+            maze[curr.r()][curr.c()+1] = '#';
+        }
+        
 ///         If you can move SOUTH and haven't encountered that cell yet,
 ///             then push the coordinate (r+1,c) onto the stack and update
 ///             maze[r+1][c] to indicate the algorithm has encountered it.
+        if(maze[curr.r()+1][curr.c()] != 'X' && maze[curr.r()+1][curr.c()] != '#') // if SOUTH discoverable
+        {
+            coordStack.push(Coord(curr.r()+1, curr.c()));
+            maze[curr.r()+1][curr.c()] = '#';
+        }
+        
 ///         If you can move WEST and haven't encountered that cell yet,
 ///             then push the coordinate (r,c-1) onto the stack and update
 ///             maze[r][c-1] to indicate the algorithm has encountered it.
+        if(maze[curr.r()][curr.c()-1] != 'X' && maze[curr.r()][curr.c()-1] != '#') // if WEST discoverable
+        {
+            coordStack.push(Coord(curr.r(), curr.c()-1));
+            maze[curr.r()][curr.c()-1] = '#';
+        }
+        
 ///         If you can move NORTH and haven't encountered that cell yet,
 ///             then push the coordinate (r-1,c) onto the stack and update
 ///             maze[r-1][c] to indicate the algorithm has encountered it.
-/// }
-/// There was no solution, so return false
+        if(maze[curr.r()-1][curr.c()] != 'X' && maze[curr.r()-1][curr.c()] != '#') // if NORTH discoverable
+        {
+            coordStack.push(Coord(curr.r()-1, curr.c()));
+            maze[curr.r()-1][curr.c()] = '#';
+        }
 
-bool pathExists(char maze[][10], int sr, int sc, int er, int ec)
-{
+    }
     return false;
 }
 
-///////////////////////////////////////////////////////////////////////////
-//  main function
-///////////////////////////////////////////////////////////////////////////
-
-#include <iostream>
-#include <stack>
-
-using namespace std;
-
-int main()
-{
-    char maze[10][10] = {
-        { 'X','X','X','X','X','X','X','X','X','X' },
-        { 'X','.','.','.','X','.','.','X','.','X' },
-        { 'X','.','X','X','X','.','.','.','.','X' },
-        { 'X','.','X','.','X','X','X','X','.','X' },
-        { 'X','X','X','.','.','.','.','.','.','X' },
-        { 'X','.','.','.','X','.','X','X','.','X' },
-        { 'X','.','X','.','X','.','.','X','.','X' },
-        { 'X','.','X','X','X','X','.','X','.','X' },
-        { 'X','.','.','X','.','.','.','X','.','X' },
-        { 'X','X','X','X','X','X','X','X','X','X' }
-    };
-    
-    if (pathExists(maze, 4,3, 1,8))
-        cout << "Solvable!" << endl;
-    else
-        cout << "Out of luck!" << endl;
-
-}
