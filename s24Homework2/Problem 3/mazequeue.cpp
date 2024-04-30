8ff541e6400e3614890f0f6cd7c498e71b78eded
Cameron Maiden <cmaiden42@g.ucla.edu>
2024-04-30 16:45:13 -0700

    *Rearranged the code in each file based on the requirement of the spec

--- s24Homework2/Problem 3/mazequeue.cpp
+++ s24Homework2/Problem 3/mazequeue.cpp
@@ -4,6 +4,10 @@
 //
 //  Created by Cameron Maiden on 4/30/24.
 //
+#include <iostream>
+#include <queue>
+
+using namespace std;
 
 ///////////////////////////////////////////////////////////////////////////
 //  Function declarations
@@ -12,38 +16,6 @@
 bool pathExists(char maze[][10], int sr, int sc, int er, int ec);
          // Return true if there is a path from (sr,sc) to (er,ec)
          // through the maze; return false otherwise
-
-///////////////////////////////////////////////////////////////////////////
-//  main function (COMMENT THE FUNCTION OUT BEFORE TURNING IT IN!!!!!!)
-///////////////////////////////////////////////////////////////////////////
-
-#include <iostream>
-#include <queue>
-
-using namespace std;
-
-//int main()
-//{
-//    char maze[10][10] = {
-//        // 0   1   2   3   4   5   6   7   8   9
-//        { 'X','X','X','X','X','X','X','X','X','X' }, // 0
-//        { 'X','.','.','.','X','.','.','X','.','X' }, // 1
-//        { 'X','.','X','X','X','.','.','.','.','X' }, // 2
-//        { 'X','.','X','.','X','X','X','X','.','X' }, // 3
-//        { 'X','X','X','.','.','.','.','.','.','X' }, // 4
-//        { 'X','.','.','.','X','.','X','X','.','X' }, // 5
-//        { 'X','.','X','.','X','.','.','X','.','X' }, // 6
-//        { 'X','.','X','X','X','X','.','X','.','X' }, // 7
-//        { 'X','.','.','X','.','.','.','X','.','X' }, // 8
-//        { 'X','X','X','X','X','X','X','X','X','X' }  // 9
-//    };
-//    
-//    if (pathExists(maze, 4,3, 1,8))
-//        cout << "Solvable!" << endl;
-//    else
-//        cout << "Out of luck!" << endl;
-//    
-//}
 
 ///////////////////////////////////////////////////////////////////////////
 //  Class implementations
@@ -79,44 +51,71 @@ bool pathExists(char maze[][10], int sr, int sc, int er, int ec)
         if (curr.r() == er && curr.c() == ec) // if the current coordinate is equal to the ending coordinate
             return true;
         
-// Check each place you can move from the current cell as follows:
-///         If you can move EAST and haven't encountered that cell yet,
-///             then push the coordinate (r,c+1) onto the stack and update
-///             maze[r][c+1] to indicate the algorithm has encountered it.
+        // Check each place you can move from the current cell as follows:
+        ///         If you can move EAST and haven't encountered that cell yet,
+        ///             then push the coordinate (r,c+1) onto the stack and update
+        ///             maze[r][c+1] to indicate the algorithm has encountered it.
         if(maze[curr.r()][curr.c()+1] != 'X' && maze[curr.r()][curr.c()+1] != '#') // if EAST discoverable
         {
             coordQueue.push(Coord(curr.r(), curr.c()+1));
             maze[curr.r()][curr.c()+1] = '#';
         }
         
-///         If you can move SOUTH and haven't encountered that cell yet,
-///             then push the coordinate (r+1,c) onto the stack and update
-///             maze[r+1][c] to indicate the algorithm has encountered it.
+        ///         If you can move SOUTH and haven't encountered that cell yet,
+        ///             then push the coordinate (r+1,c) onto the stack and update
+        ///             maze[r+1][c] to indicate the algorithm has encountered it.
         if(maze[curr.r()+1][curr.c()] != 'X' && maze[curr.r()+1][curr.c()] != '#') // if SOUTH discoverable
         {
             coordQueue.push(Coord(curr.r()+1, curr.c()));
             maze[curr.r()+1][curr.c()] = '#';
         }
         
-///         If you can move WEST and haven't encountered that cell yet,
-///             then push the coordinate (r,c-1) onto the stack and update
-///             maze[r][c-1] to indicate the algorithm has encountered it.
+        ///         If you can move WEST and haven't encountered that cell yet,
+        ///             then push the coordinate (r,c-1) onto the stack and update
+        ///             maze[r][c-1] to indicate the algorithm has encountered it.
         if(maze[curr.r()][curr.c()-1] != 'X' && maze[curr.r()][curr.c()-1] != '#') // if WEST discoverable
         {
             coordQueue.push(Coord(curr.r(), curr.c()-1));
             maze[curr.r()][curr.c()-1] = '#';
         }
         
-///         If you can move NORTH and haven't encountered that cell yet,
-///             then push the coordinate (r-1,c) onto the stack and update
-///             maze[r-1][c] to indicate the algorithm has encountered it.
+        ///         If you can move NORTH and haven't encountered that cell yet,
+        ///             then push the coordinate (r-1,c) onto the stack and update
+        ///             maze[r-1][c] to indicate the algorithm has encountered it.
         if(maze[curr.r()-1][curr.c()] != 'X' && maze[curr.r()-1][curr.c()] != '#') // if NORTH discoverable
         {
             coordQueue.push(Coord(curr.r()-1, curr.c()));
             maze[curr.r()-1][curr.c()] = '#';
         }
-
+        
     }
     return false;
 }
+
+///////////////////////////////////////////////////////////////////////////
+//  main function (COMMENT THE FUNCTION OUT BEFORE TURNING IT IN!!!!!!)
+///////////////////////////////////////////////////////////////////////////
+
+//int main()
+//{
+//    char maze[10][10] = {
+//        // 0   1   2   3   4   5   6   7   8   9
+//        { 'X','X','X','X','X','X','X','X','X','X' }, // 0
+//        { 'X','.','.','.','X','.','.','X','.','X' }, // 1
+//        { 'X','.','X','X','X','.','.','.','.','X' }, // 2
+//        { 'X','.','X','.','X','X','X','X','.','X' }, // 3
+//        { 'X','X','X','.','.','.','.','.','.','X' }, // 4
+//        { 'X','.','.','.','X','.','X','X','.','X' }, // 5
+//        { 'X','.','X','.','X','.','.','X','.','X' }, // 6
+//        { 'X','.','X','X','X','X','.','X','.','X' }, // 7
+//        { 'X','.','.','X','.','.','.','X','.','X' }, // 8
+//        { 'X','X','X','X','X','X','X','X','X','X' }  // 9
+//    };
+//
+//    if (pathExists(maze, 4,3, 1,8))
+//        cout << "Solvable!" << endl;
+//    else
+//        cout << "Out of luck!" << endl;
+//
+//}
 
--- s24Homework2/Problem 5/eval.cpp
+++ s24Homework2/Problem 5/eval.cpp
@@ -6,6 +6,19 @@
 //
 
 #include <iostream>
+#include <string>
+
+using namespace std;
+
+int evaluate(string infix, string& postfix, bool& result);
+          // Evaluates a boolean expression
+          //   If infix is a syntactically valid infix boolean expression,
+          //   then set postfix to the postfix form of that expression, set
+          //   result to the value of the expression and return zero.  If
+          //   infix is not a syntactically valid expression, return 1; in
+          //   that case, postfix may or may not be changed, but result must
+          //   be unchanged.
+
 
 int main()
 {
