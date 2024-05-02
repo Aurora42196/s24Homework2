f366b1489cd5360ddf5cf01fc91160fa88435fd4
Cameron Maiden <cmaiden42@g.ucla.edu>
2024-05-01 18:24:54 -0700

    *Commented out the main function before turning it in to the professor as required in the spec

--- s24Homework2/Problem 5/eval.cpp
+++ s24Homework2/Problem 5/eval.cpp
@@ -374,85 +374,85 @@ bool postfixEval(string& pf)
 //  main function (COMMENT THE FUNCTION OUT BEFORE TURNING IT IN!!!!!!)
 ///////////////////////////////////////////////////////////////////////////
 
-int main()
-{
-    string pf;
-    bool answer;
-    assert(evaluate("!!(F|T)", pf, answer) == 0 && pf == "FT|!!" && answer);
-    assert(evaluate("T&!F", pf, answer) == 0 && answer);
-    assert(evaluate("T|F&F", pf, answer) == 0 && answer);
-    assert(evaluate("!F|T", pf, answer) == 0  && answer);
-    assert(evaluate("T", pf, answer) == 0 && pf == "T" && answer);
-    assert(evaluate("T & !(F | T & T | F) | !!!(F & T & F) ", pf, answer) == 0 && answer);
-    assert(evaluate("T", pf, answer) == 0 && pf == "T" && answer);
-    assert(evaluate("F", pf, answer) == 0 && pf == "F" && !answer);
-    assert(evaluate("!", pf, answer) == 1);
-    assert(evaluate("|", pf, answer) == 1);
-    assert(evaluate("&", pf, answer) == 1);
-    assert(evaluate("(", pf, answer) == 1);
-    assert(evaluate(")", pf, answer) == 1);
-    assert(evaluate("", pf, answer) == 1);
-    assert(evaluate("A", pf, answer) == 1);
-    assert(evaluate("TF", pf, answer) == 1);
-    assert(evaluate(" !    !!!!!! F  ", pf, answer) == 0);
-    assert(evaluate("T|F", pf, answer) == 0 && pf == "TF|" && answer);
-    assert(evaluate(" T | F ", pf, answer) == 0 && pf == "TF|" && answer);
-    assert(evaluate("T&F", pf, answer) == 0 && pf == "TF&" && !answer);
-    assert(evaluate(" T & F ", pf, answer) == 0 && pf == "TF&" && !answer);
-    assert(evaluate("!T", pf, answer) == 0 && pf == "T!" && !answer);
-    assert(evaluate(" ! T ", pf, answer) == 0 && pf == "T!" && !answer);
-    assert(evaluate("(T)", pf, answer) == 0 && pf == "T" && answer);
-    assert(evaluate(" ( T ) ", pf, answer) == 0 && pf == "T" && answer);
-    assert(evaluate(" ( T & ! ( F | T & T & ( ! F | F ) ) | F )", pf, answer) == 0 && pf == "TFTT&F!F|&|!&F|" && !answer);
-    assert(evaluate(" T ", pf, answer) == 0);
-    assert(evaluate(" T &   F    ", pf, answer) == 0 && pf == "TF&" && !answer);
-    assert(evaluate("(T&(T&(T&(T&(T&(T&(T&(T&(T)))))))))", pf, answer) == 0 && pf == "TTTTTTTTT&&&&&&&&");
-    assert(evaluate(" ", pf, answer) == 1);
-    assert(evaluate(" T !", pf, answer) == 1);
-    assert(evaluate(" T | &", pf, answer) == 1);
-    assert(evaluate(" ( ) ", pf, answer) == 1);
-    assert(evaluate(" T ( F & F )", pf, answer) == 1);
-    assert(evaluate("T | (F&F|)", pf, answer) == 1);
-    assert(evaluate(" T ( & F ) ", pf, answer) == 1);
-//    cout << "checkpoint" << endl;
-    assert(evaluate(" F | (T) ) ", pf, answer) == 1);
-    assert(evaluate("(((((F)))))", pf, answer) == 0 && pf == "F" && !answer);
-    assert(evaluate("T| F", pf, answer) == 0 && pf == "TF|" && answer);
-    assert(evaluate("", pf, answer) == 1);
-    assert(evaluate("T|", pf, answer) == 1);
-    assert(evaluate("F F", pf, answer) == 1);
-    assert(evaluate("TF", pf, answer) == 1);
-    assert(evaluate("()", pf, answer) == 1);
-    assert(evaluate("()T", pf, answer) == 1);
-    assert(evaluate("T(F|T)", pf, answer) == 1);
-    assert(evaluate("T(&T)", pf, answer) == 1);
-    assert(evaluate("(T&(F|F)", pf, answer) == 1);
-    assert(evaluate("T+F", pf, answer) == 1);
-    assert(evaluate("F  |  !F & (T&F) ", pf, answer) == 0
-           && pf == "FF!TF&&|" && !answer);
-    assert(evaluate(" F  ", pf, answer) == 0 && pf == "F" && !answer);
-    assert(evaluate("((T))", pf, answer) == 0 && pf == "T" && answer);
+//int main()
+//{
+//    string pf;
+//    bool answer;
+//    assert(evaluate("!!(F|T)", pf, answer) == 0 && pf == "FT|!!" && answer);
+//    assert(evaluate("T&!F", pf, answer) == 0 && answer);
+//    assert(evaluate("T|F&F", pf, answer) == 0 && answer);
+//    assert(evaluate("!F|T", pf, answer) == 0  && answer);
+//    assert(evaluate("T", pf, answer) == 0 && pf == "T" && answer);
+//    assert(evaluate("T & !(F | T & T | F) | !!!(F & T & F) ", pf, answer) == 0 && answer);
+//    assert(evaluate("T", pf, answer) == 0 && pf == "T" && answer);
+//    assert(evaluate("F", pf, answer) == 0 && pf == "F" && !answer);
+//    assert(evaluate("!", pf, answer) == 1);
+//    assert(evaluate("|", pf, answer) == 1);
+//    assert(evaluate("&", pf, answer) == 1);
+//    assert(evaluate("(", pf, answer) == 1);
+//    assert(evaluate(")", pf, answer) == 1);
+//    assert(evaluate("", pf, answer) == 1);
+//    assert(evaluate("A", pf, answer) == 1);
+//    assert(evaluate("TF", pf, answer) == 1);
+//    assert(evaluate(" !    !!!!!! F  ", pf, answer) == 0);
+//    assert(evaluate("T|F", pf, answer) == 0 && pf == "TF|" && answer);
+//    assert(evaluate(" T | F ", pf, answer) == 0 && pf == "TF|" && answer);
+//    assert(evaluate("T&F", pf, answer) == 0 && pf == "TF&" && !answer);
+//    assert(evaluate(" T & F ", pf, answer) == 0 && pf == "TF&" && !answer);
+//    assert(evaluate("!T", pf, answer) == 0 && pf == "T!" && !answer);
+//    assert(evaluate(" ! T ", pf, answer) == 0 && pf == "T!" && !answer);
+//    assert(evaluate("(T)", pf, answer) == 0 && pf == "T" && answer);
+//    assert(evaluate(" ( T ) ", pf, answer) == 0 && pf == "T" && answer);
+//    assert(evaluate(" ( T & ! ( F | T & T & ( ! F | F ) ) | F )", pf, answer) == 0 && pf == "TFTT&F!F|&|!&F|" && !answer);
+//    assert(evaluate(" T ", pf, answer) == 0);
+//    assert(evaluate(" T &   F    ", pf, answer) == 0 && pf == "TF&" && !answer);
+//    assert(evaluate("(T&(T&(T&(T&(T&(T&(T&(T&(T)))))))))", pf, answer) == 0 && pf == "TTTTTTTTT&&&&&&&&");
+//    assert(evaluate(" ", pf, answer) == 1);
+//    assert(evaluate(" T !", pf, answer) == 1);
+//    assert(evaluate(" T | &", pf, answer) == 1);
+//    assert(evaluate(" ( ) ", pf, answer) == 1);
+//    assert(evaluate(" T ( F & F )", pf, answer) == 1);
+//    assert(evaluate("T | (F&F|)", pf, answer) == 1);
+//    assert(evaluate(" T ( & F ) ", pf, answer) == 1);
+////    cout << "checkpoint" << endl;
+//    assert(evaluate(" F | (T) ) ", pf, answer) == 1);
+//    assert(evaluate("(((((F)))))", pf, answer) == 0 && pf == "F" && !answer);
+//    assert(evaluate("T| F", pf, answer) == 0 && pf == "TF|" && answer);
+//    assert(evaluate("", pf, answer) == 1);
+//    assert(evaluate("T|", pf, answer) == 1);
+//    assert(evaluate("F F", pf, answer) == 1);
+//    assert(evaluate("TF", pf, answer) == 1);
+//    assert(evaluate("()", pf, answer) == 1);
+//    assert(evaluate("()T", pf, answer) == 1);
+//    assert(evaluate("T(F|T)", pf, answer) == 1);
+//    assert(evaluate("T(&T)", pf, answer) == 1);
+//    assert(evaluate("(T&(F|F)", pf, answer) == 1);
+//    assert(evaluate("T+F", pf, answer) == 1);
+//    assert(evaluate("F  |  !F & (T&F) ", pf, answer) == 0
+//           && pf == "FF!TF&&|" && !answer);
+//    assert(evaluate(" F  ", pf, answer) == 0 && pf == "F" && !answer);
+//    assert(evaluate("((T))", pf, answer) == 0 && pf == "T" && answer);
+////    cout << "Passed all tests" << endl;
+//    
+//    assert(evaluate("!(T&F|T)", pf, answer) == 0 && pf == "TF&T|!" && !answer);
+//    assert(evaluate("(T|F)&(F|F)&(!F|F&T)&T", pf, answer) == 0 && pf == "TF|FF|&F!FT&|&T&" && !answer);
+//    assert(evaluate("!(T|F) | F | F | T&F | T & !F", pf, answer) == 0 && pf == "TF|!F|F|TF&|TF!&|" && answer);
+//    assert(evaluate("T&T&T&T&T&T&T&T&T&T&F", pf, answer) == 0 && pf == "TT&T&T&T&T&T&T&T&T&F&" && !answer);
+//    assert(evaluate("(T|F)&(F|T)&(T&T)&!(T&F)|F&F", pf, answer) == 0 && pf == "TF|FT|&TT&&TF&!&FF&|" && answer);
+//    assert(evaluate("!!(T|F)&F|T&!T&!F|F|T", pf, answer) == 0 && pf == "TF|!!F&TT!&F!&|F|T|" && answer);
+//    assert(evaluate("T", pf, answer) == 0 && pf == "T" && answer);
+//    assert(evaluate("(F)", pf, answer) == 0 && pf == "F" && !answer);
+//    assert(evaluate("T&(F) ", pf, answer) == 0 && pf == "TF&" && !answer);
+//    assert(evaluate("T & !F", pf, answer) == 0 && pf == "TF!&" && answer);
+//    assert(evaluate("  !(F|T)", pf, answer) == 0 && pf == "FT|!" && !answer);
+//    assert(evaluate("!F | T", pf, answer) == 0 && pf == "F!T|" && answer);
+//    assert(evaluate("T|F&F", pf, answer) == 0 && pf == "TFF&|" && answer);
+//    assert(evaluate("T&!(F|T&T|F)|!!!(F&T&F)", pf, answer) == 0 && pf == "TFTT&|F|!&FT&F&!!!|" && answer);
+//    assert(evaluate("!(!T&(!F|T)", pf, answer) == 1);
+//    assert(evaluate("!(!F|T)&(!T|!F)|!!!F&!T&T&!(!F|T)", pf, answer) == 0 && pf == "F!T|!T!F!|&F!!!T!&T&F!T|!&|" && !answer);
+//    assert(evaluate("!!(!F|T)&(!T|!F)|!!!F&T&T&!(!F|T)", pf, answer) == 0 && pf == "F!T|!!T!F!|&F!!!T&T&F!T|!&|" && answer);
+//    assert(evaluate("!", pf, answer) == 1);
+//    assert(evaluate("F|!F&(!F & !!!F & (!F & T) & (!!T))", pf, answer) == 0 && pf == "FF!F!F!!!&F!T&&T!!&&|" && answer);
+//    
 //    cout << "Passed all tests" << endl;
-    
-    assert(evaluate("!(T&F|T)", pf, answer) == 0 && pf == "TF&T|!" && !answer);
-    assert(evaluate("(T|F)&(F|F)&(!F|F&T)&T", pf, answer) == 0 && pf == "TF|FF|&F!FT&|&T&" && !answer);
-    assert(evaluate("!(T|F) | F | F | T&F | T & !F", pf, answer) == 0 && pf == "TF|!F|F|TF&|TF!&|" && answer);
-    assert(evaluate("T&T&T&T&T&T&T&T&T&T&F", pf, answer) == 0 && pf == "TT&T&T&T&T&T&T&T&T&F&" && !answer);
-    assert(evaluate("(T|F)&(F|T)&(T&T)&!(T&F)|F&F", pf, answer) == 0 && pf == "TF|FT|&TT&&TF&!&FF&|" && answer);
-    assert(evaluate("!!(T|F)&F|T&!T&!F|F|T", pf, answer) == 0 && pf == "TF|!!F&TT!&F!&|F|T|" && answer);
-    assert(evaluate("T", pf, answer) == 0 && pf == "T" && answer);
-    assert(evaluate("(F)", pf, answer) == 0 && pf == "F" && !answer);
-    assert(evaluate("T&(F) ", pf, answer) == 0 && pf == "TF&" && !answer);
-    assert(evaluate("T & !F", pf, answer) == 0 && pf == "TF!&" && answer);
-    assert(evaluate("  !(F|T)", pf, answer) == 0 && pf == "FT|!" && !answer);
-    assert(evaluate("!F | T", pf, answer) == 0 && pf == "F!T|" && answer);
-    assert(evaluate("T|F&F", pf, answer) == 0 && pf == "TFF&|" && answer);
-    assert(evaluate("T&!(F|T&T|F)|!!!(F&T&F)", pf, answer) == 0 && pf == "TFTT&|F|!&FT&F&!!!|" && answer);
-    assert(evaluate("!(!T&(!F|T)", pf, answer) == 1);
-    assert(evaluate("!(!F|T)&(!T|!F)|!!!F&!T&T&!(!F|T)", pf, answer) == 0 && pf == "F!T|!T!F!|&F!!!T!&T&F!T|!&|" && !answer);
-    assert(evaluate("!!(!F|T)&(!T|!F)|!!!F&T&T&!(!F|T)", pf, answer) == 0 && pf == "F!T|!!T!F!|&F!!!T&T&F!T|!&|" && answer);
-    assert(evaluate("!", pf, answer) == 1);
-    assert(evaluate("F|!F&(!F & !!!F & (!F & T) & (!!T))", pf, answer) == 0 && pf == "FF!F!F!!!&F!T&&T!!&&|" && answer);
-    
-    cout << "Passed all tests" << endl;
-}
+//}
